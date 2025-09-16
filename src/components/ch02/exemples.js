20ee91eca929563182396cb72123fbd568372ed9
imrane88 <imrane.business01@gmail.com>
2025-09-16 18:23:14 -0400

    Solution exercice.

--- src/components/ch02/exemples.js
+++ src/components/ch02/exemples.js
@@ -15,3 +15,22 @@ const sum = (a,b,c,d) => {
 }
 
 console.log(sum(...numbers));
+
+
+const concatenate = (obj1, obj2) => {
+    return {
+        name1: obj1.name,
+        name2: obj2.name
+    }
+}
+
+// test concatenate 
+const obj1 = {
+    name: "Alex",
+    position: "Software developer"
+}
+const obj2 = {
+    name: "Rami",
+    position: "Designer"
+}
+console.log(concatenate(obj1, obj2));
