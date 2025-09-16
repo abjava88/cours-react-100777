1e53028f6e21c3fa5932ddf5801a46824469f314
imrane88 <imrane.business01@gmail.com>
2025-09-16 17:45:55 -0400

    initial commit.

---
+++ src/components/ch02/exemples.js
@@ -0,0 +1,44 @@
+/**
+ * EXemple 01 fonction arrow - some des deux parametres
+ */
+
+const somme = (a,b) => a + b
+
+// exemple somme 10 + 100 resultat 110
+console.log(somme(10,100));
+
+/**
+ * exemple fonction qui retorune les deux noms a partir deux objets
+ * Format objet 
+ * person {
+ * name: "John",
+ * position: "Software engineer"
+ * }
+ */
+
+const concatenate = (obj1, obj2) => {
+    return {
+        name1: obj1.name,
+        name2: obj2.name
+    }
+}
+
+// test concatenate 
+const obj1 = {
+    name: "Alex",
+    position: "Software developer"
+}
+const obj2 = {
+    name: "Rami",
+    position: "Designer"
+}
+console.log(concatenate(obj1, obj2));
+
+
+const numbers = [1,2,3,4,5,6];
+
+const sum = (a,b,c,d) => {
+    return a+b+c+d;
+}
+
+console.log(sum(...numbers));
