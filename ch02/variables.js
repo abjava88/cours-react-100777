47ed546c8e38a8a9d1d406332dfbe8e0428bdaca
imrane88 <imrane.business01@gmail.com>
2025-09-16 17:15:24 -0400

    Variables examples

--- ch02/variables.js
+++ ch02/variables.js
@@ -28,6 +28,50 @@ console.log(first);
 console.log(second);
 console.log(others);
 
-let a = "Hello";
-a[0] = "a";   // ❌ Impossible, la chaîne reste inchangée
-console.log(a); // "Hello"
+const x = 10;
+
+const car = {
+    model: 'bmw',
+    make: '2019',
+}
+
+
+
+car.make = 2025;
+car.color = 'red';
+
+console.log(car.color);
+
+console.log(`Timestamp `+ Date.now());
+
+
+// fonction fleche 
+const somme = (param1, param2) => param1 + param2;
+
+const log = (param1, param2) => {
+    return (param1 + param2 + (Date.now()/1000));
+}
+
+console.log(`log : ` + log(10,20));
+console.log(`log : ` + log(10,20));
+console.log(`log : ` + log(10,20));
+
+//
+const somme1 = function (param1, param2) {
+    return param1 + param2;
+}
+
+const compare = (param1, param2) => {
+    if(param1 > param2) return 'param1 superieur param2';
+    else return 'param2 superieur ou egal param1'
+}
+
+const fullName = 'Abbes'
+const employee = {
+    fullName
+}
+
+console.log(employee);
+
+
+
